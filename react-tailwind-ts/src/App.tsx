c787e9685a009c71abb8954bb666971e21b962fe
NandaScott <nanda1123@gmail.com>
2022-04-12 11:11:57 -0400

    Some initial tailwind stuff

--- react-tailwind-ts/src/App.tsx
+++ react-tailwind-ts/src/App.tsx
@@ -1,5 +1,29 @@
+const RightArrow = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='h-6 w-6 text-white'
+    fill='none'
+    viewBox='0 0 24 24'
+    stroke='currentColor'
+    strokeWidth={2}
+  >
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      d='M14 5l7 7m0 0l-7 7m7-7H3'
+    />
+  </svg>
+);
+
 function App() {
-  return <h1 className='bg-slate-600 mt-4'>Hello World</h1>;
+  return (
+    <div className='bg-slate-500 h-full flex items-center justify-center flex-col'>
+      <h1 className='text-white font-sans font-bold text-6xl'>
+        Exchange - o - matic
+      </h1>
+      <RightArrow />
+    </div>
+  );
 }
 
 export default App;
