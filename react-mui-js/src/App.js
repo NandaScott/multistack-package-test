0f4aca7679dea937971a885337c285b0f180a11b
NandaScott <nanda1123@gmail.com>
2022-05-09 14:04:33 -0400

    Updated react-mui-js

--- react-mui-js/src/App.js
+++ react-mui-js/src/App.js
@@ -1,88 +1,218 @@
-import React, { useCallback, useRef, useState } from 'react';
-import {
-  Autocomplete,
-  Button,
-  Paper,
-  TextField,
-  Typography,
-} from '@mui/material';
-import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
-import { currencyList, getExchangeRate } from 'nds-common';
-import invert from 'lodash/invert';
+import QuestionResolver from './components/question-resolver';
+import { useMIP, checkout } from 'nds-common';
+import { FormPage } from 'nds-common/components';
+import { Button, Typography } from '@mui/material';
 import './App.css';
-import ExchangeRateDisplay from './ExchangeRateDisplay';
 
-function App() {
-  const [from, setFrom] = useState('');
-  const [to, setTo] = useState('');
-  const [exchangeRate, setExchangeRate] = useState();
-  const currencies = useRef(
-    Object.entries(currencyList).map(([k, v]) => ({
-      label: v,
-      code: k.toUpperCase(),
-    }))
-  );
-  const codes = useRef(invert(currencyList));
-
-  const onSubmit = useCallback(
-    (e) => {
-      e.preventDefault();
-      if (!from || !to) return;
-      getExchangeRate('latest', from, to)
-        .then((resp) => setExchangeRate(resp.data))
-        .catch(console.error);
-    },
-    [from, to]
-  );
-
-  const handleChange = useCallback(
-    (input) => (e) => {
-      if (input === 'from') {
-        setFrom(codes.current[e.target.textContent]);
-      }
-      if (input === 'to') {
-        setTo(codes.current[e.target.textContent]);
-      }
-    },
-    []
-  );
+export default function App() {
+  const {
+    currentPage,
+    submitForm,
+    register,
+    handleSubmit,
+    isLastPage,
+    isFirstPage,
+    nextPage,
+    previousPage,
+    getValues,
+    watch,
+    formState: { errors },
+  } = useMIP('checkout');
+  const sameBilling = watch('address.sameBilling');
 
   return (
     <div className='main-container'>
-      <Typography className='title' variant='h2'>
-        Exchange Rate - o - matic
-      </Typography>
-      <Paper className='paper'>
-        <form onSubmit={onSubmit}>
-          <div className='controls'>
-            <Autocomplete
-              autoHighlight
-              className='autocomplete'
-              onChange={handleChange('from')}
-              options={currencies.current}
-              renderInput={(params) => <TextField {...params} label='from' />}
+      <form onSubmit={handleSubmit(submitForm)}>
+        <FormPage
+          currentPage={currentPage}
+          pageId='address'
+          className='form-page'
+        >
+          <div className='form-page__container'>
+            <Typography variant='h5' className='col-span-2'>
+              Contact Info
+            </Typography>
+            <QuestionResolver
+              questions={checkout.address}
+              register={register}
+              sliceQuestions={{ end: 3 }}
+              errors={errors}
+              pageId='address'
             />
-            <ArrowRightAltIcon
-              fontSize='large'
-              className='arrow-icon'
-              color='primary'
-            />
-            <Autocomplete
-              autoHighlight
-              className='autocomplete'
-              onChange={handleChange('to')}
-              options={currencies.current}
-              renderInput={(params) => <TextField {...params} label='to' />}
-            />
-            <Button type='submit' variant='contained'>
-              Submit
-            </Button>
           </div>
-        </form>
-      </Paper>
-      <ExchangeRateDisplay from={from} to={to} exchangeRate={exchangeRate} />
+          <div className='form-page__container mt-4'>
+            <Typography variant='h5' className='col-span-2'>
+              Shipping Info
+            </Typography>
+            <QuestionResolver
+              questions={checkout.address}
+              register={register}
+              sliceQuestions={{ start: 3, end: 9 }}
+              errors={errors}
+              watch={watch}
+              pageId='address'
+            />
+          </div>
+          {!sameBilling && (
+            <div className='form-page__container mt-4'>
+              <Typography variant='h5' className='col-span-2'>
+                Billing Info
+              </Typography>
+              <QuestionResolver
+                questions={checkout.address}
+                register={register}
+                sliceQuestions={{ start: 9 }}
+                errors={errors}
+                pageId='address'
+              />
+            </div>
+          )}
+        </FormPage>
+        <FormPage
+          pageId='ccinfo'
+          currentPage={currentPage}
+          className='form-page'
+        >
+          <div className='form-page__container'>
+            <Typography variant='h5' className='col-span-2'>
+              Payment Info
+            </Typography>
+            <QuestionResolver
+              errors={errors}
+              questions={checkout.ccinfo}
+              register={register}
+              pageId='ccinfo'
+            />
+          </div>
+        </FormPage>
+        <FormPage
+          pageId='upsell'
+          currentPage={currentPage}
+          className='form-page'
+        >
+          <Typography variant='h6' className='col-span-2'>
+            Can we tempt you with a cookie?
+          </Typography>
+          <Typography variant='subtitle1' paragraph className='col-span-2'>
+            We promise they are tasty
+          </Typography>
+          <QuestionResolver
+            errors={errors}
+            questions={checkout.upsell}
+            register={register}
+            pageId='upsell'
+          />
+        </FormPage>
+        <FormPage
+          pageId='confirmation'
+          currentPage={currentPage}
+          className='form-page grid-cols-3 items-start gap-x-4'
+        >
+          <div className='confirmation-page__container'>
+            <Typography variant='h5' className='col-span-2'>
+              Contact Info
+            </Typography>
+            <Typography paragraph className='confirmation-page__text-label'>
+              First Name
+            </Typography>
+            <Typography paragraph>{getValues('address.firstName')}</Typography>
+            <Typography paragraph className='confirmation-page__text-label'>
+              Last Name
+            </Typography>
+            <Typography paragraph>{getValues('address.lastName')}</Typography>
+            <Typography paragraph className='confirmation-page__text-label'>
+              Address 1
+            </Typography>
+            <Typography paragraph>
+              {getValues('address.shippingAddress')}
+            </Typography>
+            <Typography paragraph className='confirmation-page__text-label'>
+              Address 2
+            </Typography>
+            <Typography paragraph>
+              {getValues('address.shippingAddress2')}
+            </Typography>
+            <Typography paragraph className='confirmation-page__text-label'>
+              Country
+            </Typography>
+            <Typography paragraph>{getValues('address.country')}</Typography>
+            <Typography paragraph className='confirmation-page__text-label'>
+              State
+            </Typography>
+            <Typography paragraph>{getValues('address.state')}</Typography>
+            <Typography paragraph className='confirmation-page__text-label'>
+              Zip
+            </Typography>
+            <Typography paragraph>{getValues('address.zipcode')}</Typography>
+            <Typography paragraph className='confirmation-page__text-label'>
+              Same Billing
+            </Typography>
+            <Typography paragraph>
+              {getValues('address.sameBilling')}
+            </Typography>
+          </div>
+          <div className='confirmation-page__container'>
+            <Typography variant='h5' className='col-span-2'>
+              Payment Info
+            </Typography>
+            <Typography paragraph className='confirmation-page__text-label'>
+              Card Number
+            </Typography>
+            <Typography paragraph>{getValues('ccinfo.ccNumber')}</Typography>
+            <Typography paragraph className='confirmation-page__text-label'>
+              Expiration
+            </Typography>
+            <Typography paragraph>{getValues('ccinfo.ccExp')}</Typography>
+            <Typography paragraph className='confirmation-page__text-label'>
+              CVV
+            </Typography>
+            <Typography paragraph>{getValues('ccinfo.cvv')}</Typography>
+          </div>
+          <div className='confirmation-page__container'>
+            <Typography variant='h5' className='col-span-2'>
+              Upsell Info
+            </Typography>
+            <Typography paragraph className='confirmation-page__text-label'>
+              Cookies
+            </Typography>
+            <Typography paragraph>
+              {getValues('upsell.includeCookie')}
+            </Typography>
+          </div>
+        </FormPage>
+        <div className='button-controller'>
+          {!isFirstPage && (
+            <Button
+              variant='text'
+              color='secondary'
+              size='large'
+              onClick={previousPage}
+            >
+              Back
+            </Button>
+          )}
+          {!isLastPage && (
+            <Button
+              variant='contained'
+              color='primary'
+              size='large'
+              onClick={nextPage}
+            >
+              Next
+            </Button>
+          )}
+          {isLastPage && (
+            <Button
+              variant='contained'
+              color='primary'
+              size='large'
+              type='submit'
+            >
+              Next
+            </Button>
+          )}
+        </div>
+      </form>
     </div>
   );
 }
-
-export default App;
---
+++ react-mui-js/src/components/question-resolver.js
@@ -0,0 +1,113 @@
+import { ErrorMessage } from '@hookform/error-message';
+import {
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormHelperText,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Select,
+} from '@mui/material';
+import '../App.css';
+
+export default function QuestionResolver(props) {
+  const { questions, register, sliceQuestions, errors, pageId, watch } = props;
+  const { start, end } = sliceQuestions ?? { start: 0, end: questions.length };
+  return (
+    <>
+      {questions
+        .slice(start, end)
+        .map(({ name, type, displayLabel, span2Cols, options, value }, i) => {
+          if (type === 'select') {
+            return (
+              <FormControl key={`FormControl-${name}`}>
+                <InputLabel key={`InputLabel-${name}`}>
+                  {displayLabel}
+                </InputLabel>
+                <Select
+                  {...register(`${pageId}.${name}`)}
+                  label={displayLabel}
+                  key={`Select-${name}`}
+                >
+                  {options?.map(({ value, label }) => (
+                    <MenuItem key={`MenuItem-${label}-${value}`} value={value}>
+                      {label}
+                    </MenuItem>
+                  ))}
+                  <ErrorMessage
+                    key={`error-message-${name}`}
+                    errors={errors}
+                    name={`${pageId}.${name}`}
+                    render={({ message }) => (
+                      <FormHelperText key={`FormHelperText-${name}`} error>
+                        {message}
+                      </FormHelperText>
+                    )}
+                  />
+                </Select>
+              </FormControl>
+            );
+          }
+
+          if (type === 'singleCheckbox') {
+            return (
+              <FormGroup
+                key={`FormGroup-${name}`}
+                style={{ justifyContent: 'center' }}
+              >
+                <FormControlLabel
+                  key={`FormControlLabel-${name}`}
+                  // value={value}
+                  control={
+                    <Checkbox
+                      key={`Checkbox-${name}`}
+                      {...register(`${pageId}.${name}`)}
+                      checked={watch?.(`${pageId}.${name}`)}
+                    />
+                  }
+                  label={displayLabel}
+                />
+                <ErrorMessage
+                  key={`ErrorMessage-${name}`}
+                  errors={errors}
+                  name={`${pageId}.${name}`}
+                  render={({ message }) => (
+                    <FormHelperText key={`FormHelperText-${name}`} error>
+                      {message}
+                    </FormHelperText>
+                  )}
+                />
+              </FormGroup>
+            );
+          }
+
+          return (
+            <FormControl
+              key={`FormControl-${name}`}
+              variant='outlined'
+              className={span2Cols ? 'col-span-2' : 'col-span-1'}
+            >
+              <InputLabel>{displayLabel}</InputLabel>
+              <OutlinedInput
+                label={displayLabel}
+                type={type}
+                {...register(`${pageId}.${name}`)}
+              />
+              <ErrorMessage
+                key={`error-message-${name}`}
+                errors={errors}
+                name={`${pageId}.${name}`}
+                render={({ message }) => (
+                  <FormHelperText key={`FormHelperText-${name}`} error>
+                    {message}
+                  </FormHelperText>
+                )}
+              />
+            </FormControl>
+          );
+        })}
+    </>
+  );
+}
